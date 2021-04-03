919a669546f43615c9446077c13e8f2c9b0e0421
mhmdhammoud <dr.co0obra@hotmail.com>
2021-04-03 17:10:42 +0300

    tweak

--- product.php
+++ product.php
@@ -27,29 +27,29 @@ if (isset($_GET['id'])) {
 ?>
 
 <div class="container" style="min-height: 68vh">
-  <div class="row py-5">
-    <div class="col-md-6 col-xs-12">
-      <img class="image-fluid" src="./images/<?php echo "$image" ?>" alt="" style="height: 400px; width:100%;">
-    </div>
-    <div class="col-md-6 col-xs-12 pl-5">
-      <h1><?php echo "$name" ?></h1>
-      <h2><?php echo "$desc" ?></h2>
-      <h2>Tailor Name: <?php echo "$tname" ?></h2>
-
-      <h2><span class="text-primary">Size:</span> <?php echo "$size" ?></h2>
-      <div class='d-flex align-items-center'>
-        <h2>Color:
-        </h2>
-        <div
-          style="width:20px;height:20px;background-color:<?php echo "$color"; ?>;border-radius:50%;margin-left:20px;">
+    <div class="row py-5">
+        <div class="col-md-6 col-xs-12">
+            <img class="image-fluid" src="./images/<?php echo "$image" ?>" alt="" style="height: 400px; width:100%;">
         </div>
-      </div>
-      <h2 class="text-primary"><?php echo "$price" ?> $</h2>
-      <?php
+        <div class="col-md-6 col-xs-12 pl-5">
+            <h1><?php echo "$name" ?></h1>
+            <h2><?php echo "$desc" ?></h2>
+            <h2>Tailor Name: <?php echo "$tname" ?></h2>
+
+            <h2><span class="text-primary">Size:</span> <?php echo "$size" ?></h2>
+            <div class='d-flex align-items-center'>
+                <h2>Color:
+                </h2>
+                <div
+                    style="width:20px;height:20px;background-color:<?php echo "$color"; ?>;border-radius:50%;margin-left:20px;">
+                </div>
+            </div>
+            <h2 class="text-primary"><?php echo "$price" ?> $</h2>
+            <?php
 if (!isset($_SESSION['login'])) {
-    echo "<div><a href='./login.php'?>' class='btn btn-primary'>Login to order</a>
+    echo "<div><a href='./login.php' class='btn btn-primary'>Login to order</a>
     </div>";
-    } else if ($_SESSION['user_type'] !== 'tailor') {
+} else if ($_SESSION['user_type'] !== 'tailor') {
     echo "<div>
       <form method='POST' action='./actions/add_item_cart.api.php'>
         <div class='form-group mt-4'>
@@ -72,10 +72,10 @@ if (!isset($_SESSION['login'])) {
       </form>
     </div>";
 
-    }
-    ?>
-  </div>
-</div>
+}
+?>
+        </div>
+    </div>
 
 </div>
 
